cffb9891aa8113e6383bd86f78a30c0b8892f6ef
aaeas <easley.aaron89@gmail.com>
2022-01-17 22:22:42 -0600

    updated containers and added a way to clear each container

--- script.js
+++ script.js
@@ -24,35 +24,37 @@ var quiz = [
         answer: "all the above"
     }
 ]
+
 var timer = 60
 var score = 0
 
 // First we GRAB a reference (the element on the HTML page)
 var startBtn = document.getElementById("start");
-var titleContainer = document.getElementById('title-container');
-var choicesContainer = document.getElementById('choices-container');
-var timerContainer = document.getElementById('timer container');
-var timer = document.getElementById("timer");
+var questionContainer = document.getElementById('question-container');
+var time = document.getElementById("timer");
 var question = document.querySelector("#question");
 var answerButtons = document.querySelector("#answer-buttons");
 var container = document.getElementById("question-container");
 
 
 // Second we attach an EVENT LISTENER to the element
-startBtn.addEventListener('click', funtion() {
+startBtn.addEventListener('click', function () {
     startBtn.setAttribute('class', 'hidden')
     instructions.setAttribute("class", "hidden")
+    questionContainer.removeAttribute("class")
     displayTitleOne()
     displayTime(60)
     startTimer()
-})
+});
+
 function displayTime(timeRemaining) {
-    timerContainer.innerHTML = timeRemaining
+    time.textContent = `Timer: ${timeRemaining}`;
 }
+
 function startTimer() {   // What is the function of this method(?)
     console.log("Starting Timer ...")
     var timeLeft = 60;
-    var timer = setInterval(function () {
+    var counter = setInterval(function () {
         timeLeft = timeLeft - 1;
         console.log(timeLeft);
         // Updating the data on the HTML page
@@ -60,7 +62,7 @@ function startTimer() {   // What is the function of this method(?)
         if (timeLeft <= 0) {
             console.log("Timer Ended");
             // Clear (End/Stop) Our Timer
-            clearInterval(timer);
+            clearInterval(counter);
         }
     }, 1000);
 
@@ -69,120 +71,142 @@ function startTimer() {   // What is the function of this method(?)
 function displayTitleOne() {
     // Define the logic of our game
     console.log("Button Clicked, Start Game ...");
-    titleContainer.textContent = quiz[0].title
-    for (var i = 0; i < quiz[0].options.length; i++) {
-        var choicesBtn = document.ceatedElement('button')
-        choicesBtn.setAttribute('id', quiz[1].choices[i])
-        choicesBtn.text.Content = quiz[1].options[i]
-        choicesContainer.append(choicesBtn)
+    question.textContent = quiz[0].title
 
-        optionBtn.addEventListener('click', function (event) {
+    // clear the contents of the "answer-buttons" container
+    answerButtons.innerHTML = "";
+
+    for (var i = 0; i < quiz[0].choices.length; i++) {
+        var choicesBtn = document.createElement('button')
+        choicesBtn.setAttribute('id', quiz[0].choices[i])
+        choicesBtn.textContent = quiz[0].choices[i]
+
+        choicesBtn.addEventListener('click', function (event) {
             if (event.target.id === quiz[0].answer) {
+                score += 20
+                console.log('correct');
+            } else {
+                timer -= 10
+                console.log('incorrect');
+            }
+            displayTitleTwo()
+        });
+
+        answerButtons.append(choicesBtn);
+    }
+}
+
+
+function displayTitleTwo() {
+    // Define the logic of our game
+    console.log("Button Clicked, Start Game ...");
+    question.textContent = quiz[1].title
+
+    // clear the contents of the "answer-buttons" container
+    answerButtons.innerHTML = "";
+
+    for (var i = 0; i < quiz[1].choices.length; i++) {
+        var choicesBtn = document.createElement('button')
+        choicesBtn.setAttribute('id', quiz[1].choices[i])
+        choicesBtn.textContent = quiz[1].choices[i]
+
+        choicesBtn.addEventListener('click', function (event) {
+            if (event.target.id === quiz[1].answer) {
+                score += 20
+                console.log('correct');
+            } else {
+                timer -= 10
+                console.log('incorrect');
+            }
+            displayTitleThree()
+
+        });
+
+        answerButtons.append(choicesBtn);
+    }
+}
+
+
+function displayTitleThree() {
+    console.log("Button Clicked, Start Game ...");
+    question.textContent = quiz[2].title;
+
+    // clear the contents of the "answer-buttons" container
+    answerButtons.innerHTML = "";
+
+
+
+    for (var i = 0; i < quiz[0].choices.length; i++) {
+        var choicesBtn = document.createElement('button')
+        choicesBtn.setAttribute('id', quiz[2].choices[i])
+        choicesBtn.textContent = quiz[2].choices[i]
+
+        choicesBtn.addEventListener('click', function (event) {
+            if (event.target.id === quiz[2].answer) {
+                score += 20
+                console.log('correct');
+            } else {
+                timer -= 10
+                console.log('incorrect');
+            }
+            displayTitleFour()
+        });
+        answerButtons.append(choicesBtn);
+    }
+}
+
+function displayTitleFour() {
+    console.log("Button Clicked, Start Game ...");
+    question.textContent = quiz[3].title
+
+    // clear the contents of the "answer-buttons" container
+    answerButtons.innerHTML = "";
+
+    for (var i = 0; i < quiz[3].choices.length; i++) {
+        var choicesBtn = document.createElement('button')
+        choicesBtn.setAttribute('id', quiz[3].choices[i])
+        choicesBtn.textContent = quiz[3].choices[i]
+
+        choicesBtn.addEventListener('click', function (event) {
+            if (event.target.id === quiz[3].answer) {
                 score += 20
                 console.log('corect');
             } else {
                 timer -= 10
                 console.log('incorrect');
             }
-            displayTitleTwo()
-
-        })
+            endQuiz()
+        });
+        answerButtons.append(choicesBtn);
     }
-    function displayTitleTwo() {
-        // Define the logic of our game
-        console.log("Button Clicked, Start Game ...");
-        titleContainer.textContent = quiz[0].title
-        for (var i = 0; i < quiz[0].options.length; i++) {
-            var choicesBtn = document.ceatedElement('button')
-            choicesBtn.setAttribute('id', quiz[1].choices[i])
-            choicesBtn.text.Content = quiz[1].options[i]
-            choicesContainer.append(choicesBtn)
+};
 
-            optionBtn.addEventListener('click', function (event) {
-                if (event.target.id === quiz[0].answer) {
-                    score += 20
-                    console.log('corect');
-                } else {
-                    timer -= 10
-                    console.log('incorrect');
-                }
-                displayTitleThree()
+function endQuiz() {
 
-            })
-            function displayTitleThree() {
-                console.log("Button Clicked, Start Game ...");
-                titleContainer.textContent = quiz[0].title
-                for (var i = 0; i < quiz[0].options.length; i++) {
-                    var choicesBtn = document.ceatedElement('button')
-                    choicesBtn.setAttribute('id', quiz[1].choices[i])
-                    choicesBtn.text.Content = quiz[1].options[i]
-                    choicesContainer.append(choicesBtn)
+    clearInterval(timerInterval)
+    questionContainer.textContent = ''
+    optionsContainer.textContent = ''
+    questionContainer.textContent = 'Your Final Score Is: ' + score
 
-                    optionBtn.addEventListener('click', function (event) {
-                        if (event.target.id === quiz[0].answer) {
-                            score += 20
-                            console.log('corect');
-                        } else {
-                            timer -= 10
-                            console.log('incorrect');
-                        }
-                        displayTitleFour()
+    var input = document.createElement('input')
+    input.setAttribute('placehold', 'What is your name?')
+    optionsContainer.append(input)
+    var submitBtn = document.createElement('button')
+    submitBtn.textContent = 'Submit'
+    optionsContainer.append(submitBtn)
 
-                    })
-                    function displayTitleFour() {
-                        console.log("Button Clicked, Start Game ...");
-                        titleContainer.textContent = quiz[0].title
-                        for (var i = 0; i < quiz[0].options.length; i++) {
-                            var choicesBtn = document.ceatedElement('button')
-                            choicesBtn.setAttribute('id', quiz[1].choices[i])
-                            choicesBtn.text.Content = quiz[1].options[i]
-                            choicesContainer.append(choicesBtn)
+    submitBtn.addEventListener('click', function () {
 
-                            optionBtn.addEventListener('click', function (event) {
-                                if (event.target.id === quiz[0].answer) {
-                                    score += 20
-                                    console.log('corect');
-                                } else {
-                                    timer -= 10
-                                    console.log('incorrect');
-                                }
-                                endQuiz()
-
-                            })
-
-                            function endQuiz() {
-
-                                clearInterval(timerInterval)
-                                questionContainer.textContent = ''
-                                optionsContainer.textContent = ''
-                                questionContainer.textContent = 'Your Final Score Is: ' + score
-
-
-
-                                var input = document.createElement('input')
-                                input.setAttribute('placehold', 'What is your name?')
-                                optionsContainer.append(input)
-                                var submitBtn = document.createElement('button')
-                                submitBtn.textContent = 'Submit'
-                                optionsContainer.append(submitBtn)
-
-
-
-                                submitBtn.addEventListener('click', function () {
-
-                                    var storage = JSON.parse(localStorage.getItem('highscore'))
-
-                                    if (storage === null) {
-                                        storage = []
-                                    }
-                                    var user = {
-                                        name: input.value,
-                                        currentScore: score
-                                    }
-                                    console.log(user)
-                                    storage.push(user)
-                                    localStorage.setItem('highscore', JSON.stringify(storage))
-                                })
-
-
-
+        var storage = JSON.parse(localStorage.getItem('highscore'))
+        if (storage === null) {
+            storage = []
+        }
+        var user = {
+            name: input.value,
+            currentScore: score
+        }
+        console.log(user)
+        storage.push(user)
+        localStorage.setItem('highscore', JSON.stringify(storage))
+    });
+}
