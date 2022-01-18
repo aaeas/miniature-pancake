83cc57fa794413e661679813c2b3f9248935f8b4
aaeas <easley.aaron89@gmail.com>
2022-01-17 18:21:27 -0600

    updated readme and data.js

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Web APIs Code Quiz
-Description: The user will be able to test their code knowledge by taking a timed quiz. If they get a question wrong time will be deducted from the time remaining. Once the user completes the quiz they can then log their highest score.
+Description: The user will be able to test their code knowledge by taking a timed quiz. If they get a question wrong time will be deducted from the time remaining. Once the user completes the quiz they can then log their highest scor
 
---
+++ data.js
@@ -0,0 +1,26 @@
+var questions = [
+    // This is at index 0
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    // This is at index 1
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    // This is at index 2
+    {
+        title: "String values must be enclosed within _______ when bekng assigned to variables.",
+        choices: ["commas", "curly brackes", "quotes", "parenthesis"],
+        answer: "curly brackets"
+    },
+    // This is at index 3
+    {
+        title: "Arrays in JavaSCript can be used to store_______. ",
+        choices: ["nummbers and strings", "other arrays", "booleans", "all the above"],
+        answer: "all the above"
+    }
+]
